6d07fb21854c8818e0f87443b8f39dc6e84150b3
uriel melendez <uriel.melendez@outlook.es>
2020-10-03 22:23:31 -0500

    Creating hello world controller

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,14 +1,23 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping("/helloworld")
+	@RequestMapping(path = "/helloworld")
 	public String helloWorld() {
 		
-		return "Hello world";
+		return "Hello world!!";
+	}
+	
+	
+	//hello-world-bean
+	@RequestMapping(path = "/helloworld-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Hello world!!");
 	}
 }
