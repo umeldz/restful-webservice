217600f755b4a3d3605b2a96f6aeec6e7f2e9bf6
uriel melendez <uriel.melendez@outlook.es>
2020-10-03 22:01:01 -0500

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		
+		return "Hello world";
+	}
+}
